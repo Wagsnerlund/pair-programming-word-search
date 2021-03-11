9904eae7d3676f601024091143ed7fe0084087e0
Wagsnerlund <chelseawagner17@gmail.com>
2021-03-11 03:28:21 +0000

    Use to iterate through rows and columns

--- wordsearch.js
+++ wordsearch.js
@@ -1,8 +1,14 @@
-const wordSearch = (letters, word) => { 
-    const horizontalJoin = letters.map(ls => ls.join(''))
-    for (l of horizontalJoin) {
-        if (l.includes(word)) return true
+const wordSearch = (letters, word) => {
+    const horizontalJoin = letters.map(ls => ls.join(''));
+    for (const l of horizontalJoin) {
+        if (l.includes(word)) return true;
     }
-}
-
-module.exports = wordSearch
+  
+    const verticalJoin = letters[0].map((col, i) => letters.map(row => row[i])).map(ls => ls.join(''));
+    for (const l of verticalJoin) {
+        if (l.includes(word)) return true;
+    }
+    return false;
+  }
+  
+  module.exports = wordSearch;
